0d506a1d9216f17df55d5c2091d851d520f9d637
Matt Smith <mrandolph83@gmail.com>
2020-11-20 17:08:51 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,10 +8,14 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
+  # This begins the pattern of error rescuing
+      begin
   # Raise method allows you to navigate
   # to a custom error message when certain
   # parameters are met or not met
       raise PartnerError
+    rescue PartnerError => error
+      puts error.message
     else
       person.partner = self
     end
